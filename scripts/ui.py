7d67acca48b4050a878de64bbab4e5459f0387a9
wqferr <wqferr@gmail.com>
2017-12-07 02:41:46 +0000

    Add state highlighting

--- scripts/ui.py
+++ scripts/ui.py
@@ -1,8 +1,22 @@
+from itertools import chain
 from math import pi, sin, cos, atan2
 from browser import document
 
 class Node(object):
+	NO_HIGHLIGHT = 0
+	SELECTED = 1
+	ACTIVE = 2
+
 	radius = 20
+	selection_ring_extra_radius = 4
+	selection_ring_color = '#4444DDA0'
+	selection_ring_thickness = 4
+	active_ring_extra_radius = 4
+	active_ring_color = '#DD4444A0'
+	active_ring_thickness = 4
+
+	font_name = 'Arial'
+	font_size = 16
 
 	def __init__(
 		self,
@@ -16,49 +30,87 @@ class Node(object):
 		self.border_thickness = border_thickness
 		self.fill_color = fill_color
 		self.border_color = border_color
-		self._connections = []
+		self.highlight = Node.NO_HIGHLIGHT
+		self._outgoing_connections = []
+		self._incoming_connections = []
 
 	def move_to(self, x, y):
 		self.x = x
 		self.y = y
 
-		for con in self._connections:
+		for con in chain(
+			self._outgoing_connections,
+			self._incoming_connections):
+
 			con.update()
 
-	def connect(self, other, *args):
-		self._connections.append(Connection(self, other, args))
+	def set_highlight(self, highlight):
+		self.highlight = highlight
+
+	def connect(self, other, args):
+		c = Connection(self, other, args)
+		self._outgoing_connections.append(c)
+		other._incoming_connections.append(c)
 
 	def draw(self, context):
+		context.save()
+		r = Node.radius
+
 		context.beginPath()
 		context.fillStyle = self.fill_color
+		context.arc(self.x, self.y, r, 0, 2*pi)
+		context.fill()
+		r += self.border_thickness / 2
+
+		if self.highlight & Node.SELECTED:
+			r += Node.selection_ring_extra_radius
+			context.beginPath()
+			context.lineWidth = Node.selection_ring_thickness
+			context.strokeStyle = Node.selection_ring_color
+			context.arc(self.x, self.y, r, 0, 2*pi)
+			context.stroke()
+			r += Node.selection_ring_thickness / 2
+		if self.highlight & Node.ACTIVE:
+			r += Node.active_ring_extra_radius
+			context.beginPath()
+			context.lineWidth = Node.active_ring_thickness
+			context.strokeStyle = Node.active_ring_color
+			context.arc(self.x, self.y, r, 0, 2*pi)
+			context.stroke()
+			r += Node.active_ring_thickness / 2
+
+		context.beginPath()
+		context.strokeStyle = '#000000'
+		context.lineWidth = self.border_thickness
 		context.arc(
 			self.x, self.y,
 			Node.radius,
 			0, 2*pi)
-		context.fill()
-
-		context.beginPath()
-		context.lineWidth = self.border_thickness
-		context.arc(
-			self.x, self.y,
-			self.radius,
-			0, 2*pi)
 		context.stroke()
-		# TODO draw text
 
-		for con in self._connections:
+		context.textAlign = 'center'
+		context.font = '{}pt {}'.format(Node.font_size, Node.font_name)
+		context.fillStyle = '#000000'
+		context.fillText(self.text, self.x, self.y + Node.font_size / 2)
+		context.restore()
+
+	def draw_outgoing_connections(self, context):
+		for con in self._outgoing_connections:
 			con.draw(context)
 
 class Connection(object):
 	head_width = 10
 	head_length = 15
 	head_angle = atan2(head_width/2, head_length)
-	line_thickness = 1
+	line_thickness = 2
 
 	def __init__(self, start_node, end_node, info):
 		self.start_node = start_node
 		self.end_node = end_node
-		self.info = tuple(info)
+		if info is None:
+			self.info = None
+		else:
+			self.info = tuple(info)
 		self.update()
 
 	def update(self):
@@ -91,8 +143,10 @@ class Connection(object):
 		pass
 
 	def draw(self, context):
+		context.save()
 		# TODO allow curved arrows
 		context.beginPath()
+		context.strokeStyle = '#000000'
 		context.lineWidth = Connection.line_thickness
 		context.moveTo(*self.start_point)
 		context.lineTo(*self.end_point)
@@ -101,4 +155,5 @@ class Connection(object):
 		context.moveTo(*self.head_points[1])
 		context.lineTo(*self.end_point)
 		context.stroke()
+		context.restore()
 
